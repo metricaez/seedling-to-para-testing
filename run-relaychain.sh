72120c3d94fbadb5357eb0d2e63ed82a729a39ef
metricaez <emiliano@parity.io>
2024-02-22 12:26:24 -0300

    add dicover-local

--- run-relaychain.sh
+++ run-relaychain.sh
@@ -6,14 +6,14 @@
 
 echo "Relaychain start up"
 
-./bin/polkadot-v1.4.0/polkadot build-spec\
-  --disable-default-bootnode\
-  --chain=rococo-local > ./resources/rococo-relay-local.json
+# ./bin/polkadot-v1.4.0/polkadot build-spec\
+#   --disable-default-bootnode\
+#   --chain=rococo-local > ./resources/rococo-relay-local.json
 
-./bin/polkadot-v1.4.0/polkadot build-spec\
-  --chain ./resources/rococo-relay-local.json\
-  --raw\
-  --disable-default-bootnode > ./resources/rococo-relay-local-raw.json
+# ./bin/polkadot-v1.4.0/polkadot build-spec\
+#   --chain ./resources/rococo-relay-local.json\
+#   --raw\
+#   --disable-default-bootnode > ./resources/rococo-relay-local-raw.json
 
 # RUST_LOG=runtime=trace,runtime::bridge=trace,runtime::bridge-messages=trace
 RUST_LOG=system=debug,paras=debug,nacho=debug,parachain::candidate-validation=debug,parachain::availability,runtime::inclusion,parachain::collation-generation=trace,parachain::candidate-backing=trace,parachain::bitfield-signing=trace,parachain::approval-voting=trace,parachain::availability-recovery=trace,parachain::availability-distribution=trace
@@ -29,6 +29,7 @@ export RUST_LOG
 	--rpc-port=9933\
 	--execution=wasm\
 	--rpc-cors=all\
+	--discover-local\
 	--unsafe-rpc-external &> ./logs/alice.log&
 
 ./bin/polkadot-v1.4.0/polkadot\
@@ -39,25 +40,27 @@ export RUST_LOG
 	--rpc-port=9934\
 	--execution=wasm\
 	--rpc-cors=all\
+	--discover-local\
+	--bootnodes=/ip4/127.0.0.1/tcp/30303/p2p/12D3KooWFAyoGrw5f6RFkRoBmC56U2wgTKRE4qusFezfhQdXh4R9\
 	--unsafe-rpc-external &> ./logs/bob.log&
 
-./bin/polkadot-v1.4.0/polkadot\
-    --tmp\
-	--chain=./resources/rococo-relay-local-raw.json\
-	--charlie\
-	--port=30335\
-	--rpc-port=9935\
-	--execution=wasm\
-	--rpc-cors=all\
-	--unsafe-rpc-external &> ./logs/charlie.log&
+# ./bin/polkadot-v1.4.0/polkadot\
+#     --tmp\
+# 	--chain=./resources/rococo-relay-local-raw.json\
+# 	--charlie\
+# 	--port=30335\
+# 	--rpc-port=9935\
+# 	--execution=wasm\
+# 	--rpc-cors=all\
+# 	--unsafe-rpc-external &> ./logs/charlie.log&
 
-./bin/polkadot-v1.4.0/polkadot\
-    --tmp\
-	--chain=./resources/rococo-relay-local-raw.json\
-	--dave\
-	--port=30336\
-	--rpc-port=9936\
-	--execution=wasm\
-	--rpc-cors=all\
-	--unsafe-rpc-external &> ./logs/dace.log&
+# ./bin/polkadot-v1.4.0/polkadot\
+#     --tmp\
+# 	--chain=./resources/rococo-relay-local-raw.json\
+# 	--dave\
+# 	--port=30336\
+# 	--rpc-port=9936\
+# 	--execution=wasm\
+# 	--rpc-cors=all\
+# 	--unsafe-rpc-external &> ./logs/dace.log&
 
